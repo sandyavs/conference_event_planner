85e91468f08fc85f9b4a92d6dc7d0744b844814a
“sandyavs” <sandyasadasivam01@gmail.com>
2025-07-27 13:08:26 -0400

    add-ons: store & actions added

--- src/store.js
+++ src/store.js
@@ -1,10 +1,13 @@
 // store.js - creates a global redux store
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
+import avReducer from './avSlice';
 
-// all components in the app can access the state managed by the venueReducer() 
+// all components in the app can access the state managed 
+// by venueReducer(), avReducer()
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: avReducer
   },
 });
